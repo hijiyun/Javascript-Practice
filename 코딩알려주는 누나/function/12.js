21dbbb7cab3dfbed4a3d3b88634ac00f74c57461
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-10 00:59:30 +0900

    함수문제 5번 문제 중간까지의 식

--- 코딩알려주는 누나/function/12.js
+++ 코딩알려주는 누나/function/12.js
@@ -14,39 +14,26 @@
 // const name = greet("지윤")
 // console.log("안녕, 내 이름은",name,"이야")
 
-4.
-function meetAt(year, month, date) {
-    let todayYear = year;
-    let todayMonth = month;
-    let todayDate = date;
-
-    if (todayDate) {
-        return `${todayYear}/${todayMonth}/${todayDate}`;
-    }
-    if (todayYear) {
-        return `${todayYear}년`;
-    }
-    if (todayMonth) {
-        return `${todayYear}년 ${todayMonth}월`;
-    }
-}
-
-console.log(meetAt(2022,3,10));
-
+// 4.
 // function meetAt(year, month, date) {
 //     let todayYear = year;
 //     let todayMonth = month;
 //     let todayDate = date;
 
 //     if (todayDate) {
-//     return `${todayYear}/${todayMonth}/${todayDate}`;
-//     }
-//     if (todayMonth) {
-//     return `${todayYear}년 ${todayMonth}월`;
+//         return `${todayYear}/${todayMonth}/${todayDate}`;
 //     }
 //     if (todayYear) {
-//     return `${todayYear}년`;
+//         return `${todayYear}년`;
+//     }
+//     if (todayMonth) {
+//         return `${todayYear}년 ${todayMonth}월`;
 //     }
 // }
 
-// console.log(meetAt(2022, 1, 7));
+// console.log(meetAt(2022,3,10));
+
+5.
+function findSmallestElement() {
+    let 
+}
