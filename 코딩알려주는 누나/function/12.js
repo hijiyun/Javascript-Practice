8277fb670a126efb01a863f4afdc817a2e519a24
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-10 00:55:52 +0900

    함수문제 4번까지 완료

--- 코딩알려주는 누나/function/12.js
+++ 코딩알려주는 누나/function/12.js
@@ -1,5 +1,52 @@
-1.
-function greet(name) {
-    console.log("안녕 내 이름은" ,name,"야")
+// 1.
+// 2.
+// function greet(name) {
+//     console.log("안녕 내 이름은" ,name,"이야")
+// }
+// greet("지윤")
+
+// 3.
+// function greet(name) {
+//     console.log("안녕 내 이름은", name, "이야");
+//     return name
+// }
+// //결과 확인법
+// const name = greet("지윤")
+// console.log("안녕, 내 이름은",name,"이야")
+
+4.
+function meetAt(year, month, date) {
+    let todayYear = year;
+    let todayMonth = month;
+    let todayDate = date;
+
+    if (todayDate) {
+        return `${todayYear}/${todayMonth}/${todayDate}`;
+    }
+    if (todayYear) {
+        return `${todayYear}년`;
+    }
+    if (todayMonth) {
+        return `${todayYear}년 ${todayMonth}월`;
+    }
 }
-greet("지윤")
+
+console.log(meetAt(2022,3,10));
+
+// function meetAt(year, month, date) {
+//     let todayYear = year;
+//     let todayMonth = month;
+//     let todayDate = date;
+
+//     if (todayDate) {
+//     return `${todayYear}/${todayMonth}/${todayDate}`;
+//     }
+//     if (todayMonth) {
+//     return `${todayYear}년 ${todayMonth}월`;
+//     }
+//     if (todayYear) {
+//     return `${todayYear}년`;
+//     }
+// }
+
+// console.log(meetAt(2022, 1, 7));
