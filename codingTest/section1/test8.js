b050c5f82ec809d628832227a1539c9074284548
박지윤 <tkrhkfld123@naver.com>
2023-10-30 16:59:28 +0900

    test: test8 test success

--- codingTest/section1/test8.js
+++ codingTest/section1/test8.js
@@ -1,6 +1,18 @@
 function solution(arr){
     let answer=arr;
-    
+    let sum = arr.reduce((a, b) => a+b, 0)
+    let flag = 0;
+    for (let i=0; i<arr.length-1; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if((sum-(arr[i]+arr[j]))===100){
+                arr.splice(j, 1); //j번째 있는 거 1개 제거해라.
+                arr.splice(i, 1); //i번째 있는 거 1개 제거해라.
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1) break;
+    }
     return answer;
 }
 
