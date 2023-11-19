170ad3247ba244bceadcc712a5796442ebb82da6
박지윤 <tkrhkfld123@naver.com>
2023-11-20 00:15:48 +0900

    🧪 test: section2 test2 retry

--- codingTest/section2/test2.js
+++ codingTest/section2/test2.js
@@ -1,12 +1,11 @@
-function solution(arr){         
-    let answer=1, max=arr[0];
-    for(let i=1; i<arr.length; i++){
-        if(arr[i]>max){
-            max=arr[i]
-            answer ++
-        }
-    }
-    return answer;
+function solution(arr){
+  answer = 0;
+  arr[0] ? answer += 1 : ''
+  for (let i=1; i<arr.length; i++){
+    if(arr[i]>arr[i-1]) answer += 1
+    else if(arr[i] === arr[i-1]) answer -= 1
+  }
+  return answer;
 }
 
 let arr=[130, 135, 148, 140, 145, 150, 150, 153];
