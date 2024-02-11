b3946d7e3e622ef1c86825981fc14d921b9351bc
지윤 <114905530+hijiyun@users.noreply.github.com>
2024-02-11 15:43:16 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,12 +1,14 @@
+function fractionalAddition(num, den, max){
+    for (let i=1; i<=num; i++){
+        num%i === 0 && den%i === 0 ? max = i : null
+    }
+    return [num / max, den / max]
+}
+
 function solution(numer1, denom1, numer2, denom2) {
-    let a = numer1 * denom2 + numer2 * denom1
-    let b = denom1 * denom2
+    let num = numer1 * denom2 + numer2 * denom1
+    let den = denom1 * denom2
     let maximum = 1
     
-    for (let i = 1; i <= a; i++) {
-        if(a%i === 0 && b%i === 0) {
-            maximum = i
-        }
-    }
-    return [a / maximum, b / maximum]
+    return fractionalAddition(num,den,maximum) // 분수 덧셈 함수
 }
