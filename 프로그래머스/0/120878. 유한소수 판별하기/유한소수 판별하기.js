eb5839630246843e2aa660739cea6075e09e8dd1
지윤 <114905530+hijiyun@users.noreply.github.com>
2024-02-11 16:01:03 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,15 @@
+function max(a,b,max) {
+    for (let i = 1; i<= b; i++){
+    if(a%i ===0 && b%i ===0) max = i;
+    }
+    return max
+}
+
+function solution(a, b) {
+    let maximum = 1
+    let m = max(a,b,maximum) // 최대공약수 구하기
+    b = b/m
+    while(b%2 === 0) b = b/2
+    while(b%5 === 0) b = b/5
+    return b === 1 ? 1 : 2;
+}
