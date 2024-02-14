daf7d723684bd69d61495b1e3a4ab9888833707f
지윤 <114905530+hijiyun@users.noreply.github.com>
2024-02-14 18:57:14 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,15 +1,36 @@
-function max(a,b,max) {
-    for (let i = 1; i<= b; i++){
-    if(a%i ===0 && b%i ===0) max = i;
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
     }
-    return max
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function solution(a, b) {
-    let maximum = 1
-    let m = max(a,b,maximum) // 최대공약수 구하기
-    b = b/m
-    while(b%2 === 0) b = b/2
-    while(b%5 === 0) b = b/5
-    return b === 1 ? 1 : 2;
+    const num = a < b ? a : b;
+    let arr = [];
+    
+    for (let i = 1; i <= num; i++) {
+        if (a % i === 0 && b % i === 0) {
+            arr.push(i);
+        }
+    }
+    const GCD = Math.max(...arr);
+
+    const denom = b / GCD;
+
+    let factors = [];
+    for (let i = 2; i <= denom; i++) {
+        if (denom % i === 0) {
+            factors.push(i);
+        }
+    }
+        
+    const primeFactors = factors.filter(isPrime);
+
+    return primeFactors.every(i => i === 2 || i === 5) ? 1 : 2;
 }
