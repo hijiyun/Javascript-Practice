0a9bdab556a89192e195f87c072648e2b39530b8
지윤 <114905530+hijiyun@users.noreply.github.com>
2024-04-07 13:20:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,8 @@
+//이거 왤케 안풀리냐
+function solution(s) {
+    s = s.split(" ")
+    while(s.includes('Z')) {
+        s.splice(s.indexOf('Z')-1, 2)
+    }
+    return s.reduce((a,b) => a+Number(b), 0)
+}
