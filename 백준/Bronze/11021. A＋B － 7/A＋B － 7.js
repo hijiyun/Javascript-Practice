91eb85f2562acf798004e7e588c18fea38501710
지윤 <114905530+hijiyun@users.noreply.github.com>
2024-04-11 12:26:48 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+for (let i = 1; i <= input.length - 1; i++) {
+  const [A, B] = input[i].split(" ");
+  let result = Number(A) + Number(B);
+
+  console.log(`Case #${i}: ${result}`);
+}
